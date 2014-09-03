0b7cbc53294fa811d93fd5d0e9ccacd1a5773c5c
breno250 <brenomirandaster@gmail.com>
2014-09-02 21:43:56 -0300

    phonegap at

---
+++ www/ver.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Cola</title>
+<link href="css/jquery.mobile-1.0.min.css" rel="stylesheet" type="text/css"/>
+<script src="js/jquery-1.6.4.min.js" type="text/javascript"></script>
+<script src="js/jquery.mobile-1.0.min.js" type="text/javascript"></script>
+</head> 
+<body>
+<div data-role="page" id="page">
+	<div data-role="header">
+		<h1>ColaWeb</h1>
+	</div>
+	<div data-role="content">	
+	<ul data-role="listview">
+        <li><a href="#page5">Apple</a></li>
+        <li><a href="#page5">Banana</a></li>
+        <li><a href="#page5">Cherry</a></li>
+        <li><a href="#page5">Cranberry</a></li>
+        <li><a href="#page5">Grape</a></li>
+        <li><a href="#page5">Orange</a></li>
+      </ul>
+	</div>
+	<div data-role="footer">
+		<h4><?php echo "breno mirtanda"; ?></h4>
+	</div>
+</div>
+
+</body>
+</html>
